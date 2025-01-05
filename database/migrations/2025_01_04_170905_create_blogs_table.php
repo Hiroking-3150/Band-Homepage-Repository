9a7fd32ec49b7b87286d1ec9835c445967c03cc4
Hiroking-3150 <tatehiro.10@gmail.com>
2025-01-05 16:32:14 +0000

    In order to link the blog and user tables, we migrated each table.

---
+++ database/migrations/2025_01_04_170905_create_blogs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('users_id')->nullable();
+            $table->string('title', 100);
+            $table->text('body');
+            $table->unsignedBigInteger('schedule_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
